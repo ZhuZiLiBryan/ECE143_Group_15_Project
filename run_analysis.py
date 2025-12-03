267bc81855600b359d3ea3694af5cd38e6743f1c
JoeQiu12138 <qiujj1129@gmail.com>
2025-12-02 20:21:14 -0800

    Add files via upload

--- run_analysis.py
+++ run_analysis.py
@@ -14,10 +14,14 @@ from promotional_analysis.promotional_analysis_main import promotional_analsysis
 from eda_milk_ratio.eda_milk_ratio_main import eda_milk_main
 from user_analysis import user_analysis_main
 from eda_weekday_weekend import eda_weekday_weekend_main
+from kmeans import kmeans_main
+from eda_Hours0fDay import eda_hours_main
 
 if __name__ == "__main__":
-    eda_milk_main()
-    eda_weekday_weekend_main()
     promotional_analsysis_main()
+    eda_milk_main()
     user_analysis_main()
+    eda_weekday_weekend_main()
+    kmeans_main()
+    eda_hours_main()
 
