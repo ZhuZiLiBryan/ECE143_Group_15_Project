ad50c63dc0dd37d467757390d62cb802a712707f
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2025-12-02 20:21:09 -0800

    all visualizations

---
+++ eda_milk_ratio/eda_milk_ratio_deps/__init__.py
@@ -0,0 +1,8 @@
+"""Weekday/Weekend/Holiday EDA package extracted from weekday_weekend_analysis.ipynb."""
+
+# Alias for consistent naming convention across modules
+
+__all__ = [
+    "eda_milk_main",
+]
+
